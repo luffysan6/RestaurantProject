de8bab10fe4b3dbdf646b5d3e86dae3267ec297b
Dhruv Rohilla <dhruvrohilla666@gmail.com>
2026-08-04 19:24:21 +0530

    Added Multi Image Upload to Create API and Started Update API

--- server/src/controller/food.controller.js
+++ server/src/controller/food.controller.js
@@ -4,12 +4,14 @@
 
 import fileUploader from "../config/fileUploader.js";
 import Food from "../model/food.model.js";
+import {Object} from 'mongoose'
 
 export const CreateFood = async (req, res) => {
   try {
     const { name, description, category, price, isavaiable } = req.body;
     const file = req.files; // multer.array() then req.files else multer.single then req.file
 
+    console.log(file);
     // validating Data
     if (!name || !description || !category || !price || !isavaiable) {
       return res.status(400).json({
@@ -18,7 +20,14 @@ export const CreateFood = async (req, res) => {
       });
     }
 
-    const result = await fileUploader(file[0].path, "food_Image");
+    const result = await Promise.all(
+      file.map((fileObj) =>
+        fileUploader(fileObj.path, "food_Image").then((value) => {
+          console.log(value);
+          return value.secure_url;
+        }),
+      ),
+    );
 
     const newFood = new Food();
     newFood.name = name;
@@ -26,13 +35,16 @@ export const CreateFood = async (req, res) => {
     newFood.category = category;
     newFood.price = price;
     newFood.isavaiable = isavaiable;
-    newFood.images = [result?.secure_url];
+    newFood.images = result;
 
     await newFood.save();
 
     console.log(newFood);
 
-    res.status(201).json({ newFood });
+    res.status(201).json({
+      success: true,
+      newFoodItem: newFood,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -40,3 +52,28 @@ export const CreateFood = async (req, res) => {
     });
   }
 };
+export const updateFoodData = async (req,res) => {
+    try {
+       const { name, description, category, price, isavaiable, } = req.body;
+      const {id} = req.params;
+
+
+      const foodData = await Food.findOne({_id: new Object(id)});
+
+
+      if(!foodData){
+        return res.status(404).json({
+          success:false,
+          message:"Resource Not Found"
+        })
+      }
+
+      foodData.save()
+
+    } catch (error) {
+      return res.status(500).json({
+      success: false,
+      message: "Error at Server",
+    });
+    }
+}
--- server/src/routes/food.route.js
+++ server/src/routes/food.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { CreateFood } from "../controller/food.controller.js";
+import { CreateFood, updateFoodData } from "../controller/food.controller.js";
 import multer from "../libs/multer.cjs";
 
 const router = Router();
 
 router.post("/create", multer.array("foodImage"), CreateFood);
+router.post("/updateFoodDate/:id", updateFoodData);
 
 export default router;
